33cb54494f8d821c498cec28030a3295c4a81595
Thatsaphon <thatphon123@hotmail.com>
2021-03-25 07:19:14 +0700

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { useState, useEffect } from "react"
 
 function App() {
+  const [data, setData] = useState([])
+  const [text, setText] = useState("")
+
+  useEffect(function () {
+    async function getData() {
+      try {
+        let response = await fetch("https://api.publicapis.org/categories")
+        let data = await response.json()
+        setData(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getData()
+    console.log("effect running")
+  }, [])
+
+  const handleTextChange = (e) => {
+    setText(e.target.value)
+    console.log(e.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="">
+        <div className="SearchBar">
+          <input type="text" onChange={handleTextChange} />
+        </div>
+        <div className="Container">
+          {data.map((item) =>
+            item.toLowerCase().includes(text) ? (
+              <div className="Box">{item}</div>
+            ) : (
+              <></>
+            )
+          )}
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
